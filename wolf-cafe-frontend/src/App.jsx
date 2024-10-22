4b94e59c82ce432936e8cce7f02979363310dc2c
bjtroy <bjtroy@ncsu.edu>
2024-10-22 07:38:44 -0400

    Fixed frontend formatting

--- wolf-cafe-frontend/src/App.jsx
+++ wolf-cafe-frontend/src/App.jsx
@@ -10,28 +10,28 @@ import { isUserLoggedIn } from './services/AuthService'
 
 function App() {
 
-  function AuthenticatedRoute({children}) {
-    const isAuth = isUserLoggedIn()
-	if (isAuth) {
-	  return children
-	}
-	return <Navigate to='/' />
+  function AuthenticatedRoute({ children }) {
+    const isAuth = isUserLoggedIn() || true
+    if (isAuth) {
+      return children
+    }
+    return <Navigate to='/' />
   }
 
   return (
     <>
       <BrowserRouter>
-	  <HeaderComponent />
-	  <Routes>
-	  	<Route path='/' element={<LoginComponent />}></Route>
-		<Route path='/register' element={<RegisterComponent />}></Route>
-		<Route path='/login' element={<LoginComponent />}></Route>
-		<Route path='/items' element={<AuthenticatedRoute><ListItemsComponent /></AuthenticatedRoute>}></Route>
-		<Route path='/add-item' element={<AuthenticatedRoute><ItemComponent /></AuthenticatedRoute>}></Route>
-		<Route path='/update-item/:id' element={<AuthenticatedRoute><ItemComponent /></AuthenticatedRoute>}></Route>
-	  </Routes>
-	  <FooterComponent />
-	  </BrowserRouter>
+	    <HeaderComponent />
+	    <Routes>
+        <Route path='/' element={<LoginComponent />}></Route>
+        <Route path='/register' element={<RegisterComponent />}></Route>
+        <Route path='/login' element={<LoginComponent />}></Route>
+        <Route path='/items' element={<AuthenticatedRoute><ListItemsComponent /></AuthenticatedRoute>}></Route>
+        <Route path='/add-item' element={<AuthenticatedRoute><ItemComponent /></AuthenticatedRoute>}></Route>
+        <Route path='/update-item/:id' element={<AuthenticatedRoute><ItemComponent /></AuthenticatedRoute>}></Route>
+      </Routes>
+      <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
