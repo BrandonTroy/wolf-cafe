3c0190a90e7dfa4d94ca53ada0f405603c84c5dd
bjtroy <bjtroy@ncsu.edu>
2024-11-18 14:50:34 -0500

    Revert "Testing if removing unit tests (lowering coverage) causes build to fail"
    This reverts commit 9e17626360b98f0cc518bc27281cc8ba30aad6b2.

---
+++ wolf-cafe-backend/src/test/java/edu/ncsu/csc326/wolfcafe/controller/InventoryControllerTest.java
@@ -0,0 +1,158 @@
+/**
+ *
+ */
+package edu.ncsu.csc326.wolfcafe.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.ncsu.csc326.wolfcafe.TestUtils;
+import edu.ncsu.csc326.wolfcafe.dto.InventoryDto;
+import edu.ncsu.csc326.wolfcafe.dto.ItemDto;
+import edu.ncsu.csc326.wolfcafe.repository.InventoryRepository;
+import edu.ncsu.csc326.wolfcafe.repository.ItemRepository;
+import edu.ncsu.csc326.wolfcafe.service.ItemService;
+
+/**
+ * Tests InventoryController class
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InventoryControllerTest {
+
+    /** Mock MVC for testing controller */
+    @Autowired
+    private MockMvc             mvc;
+
+    /**
+     * Connection to the table of items to delete all items
+     */
+    @Autowired
+    private ItemRepository      itemRepository;
+
+    /**
+     * Connection to the ItemService to add items that should appear in the
+     * inventory
+     */
+    @Autowired
+    private ItemService         itemService;
+
+    /**
+     * Connection to the inventory table to delete all items
+     */
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    /**
+     * Tests the GET /api/inventory endpoint.
+     *
+     * @throws Exception
+     *             if issue when running the test.
+     */
+    @Test
+    @Transactional
+    @WithMockUser ( username = "joe schmoe" )
+    public void testGetInventory () throws Exception {
+        inventoryRepository.deleteAll();
+        itemRepository.deleteAll();
+        itemRepository.flush();
+        inventoryRepository.flush();
+        final InventoryDto expectedInventory = new InventoryDto();
+
+        mvc.perform( get( "/api/inventory" ) )
+                .andExpect( content().string( TestUtils.asJsonString( expectedInventory ) ) )
+                .andExpect( status().isOk() );
+
+        final ItemDto newItem = itemService.addItem( new ItemDto( 0L, "Water", "H2O", 3.50 ) );
+        expectedInventory.getItemQuantities().put( newItem.getId(), 0 );
+
+        mvc.perform( get( "/api/inventory" ) )
+                .andExpect( content().string( TestUtils.asJsonString( expectedInventory ) ) )
+                .andExpect( status().isOk() );
+    }
+
+    /**
+     * Tests the PUT /api/inventory endpoint.
+     *
+     * @throws Exception
+     *             if issue when running the test.
+     */
+    @Test
+    @Transactional
+    @WithMockUser ( username = "staff", roles = "MANAGER" )
+    public void testUpdateInventory () throws Exception {
+        inventoryRepository.deleteAll();
+        itemRepository.deleteAll();
+        itemRepository.flush();
+        inventoryRepository.flush();
+        final InventoryDto expectedInventory = new InventoryDto();
+
+        final ItemDto newItem = itemService.addItem( new ItemDto( 0L, "Water", "H2O", 3.50 ) );
+        final ItemDto newItem2 = itemService.addItem( new ItemDto( 0L, "Coffee", "Brown liquid", 5.50 ) );
+        expectedInventory.getItemQuantities().put( newItem.getId(), 5 );
+        expectedInventory.getItemQuantities().put( newItem2.getId(), 3 );
+
+        final InventoryDto updates = new InventoryDto();
+        updates.getItemQuantities().put( newItem2.getId(), 3 );
+        updates.getItemQuantities().put( newItem.getId(), 5 );
+        mvc.perform( put( "/api/inventory" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updates ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() )
+                .andExpect( content().string( TestUtils.asJsonString( expectedInventory ) ) );
+
+        updates.getItemQuantities().put( newItem2.getId() + 1L, 3 );
+        mvc.perform( put( "/api/inventory" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updates ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isBadRequest() );
+    }
+
+    /**
+     * Tests updating the inventory if the item exists but the inventory entry
+     * got deleted
+     *
+     * @throws Exception
+     *             if issue when running the test.
+     */
+    @Test
+    @Transactional
+    @WithMockUser ( username = "staff", roles = "MANAGER" )
+    public void testUpdateInventoryNoEntry () throws Exception {
+        inventoryRepository.deleteAll();
+        itemRepository.deleteAll();
+        itemRepository.flush();
+        inventoryRepository.flush();
+        final InventoryDto expectedInventory = new InventoryDto();
+
+        final ItemDto newItem = itemService.addItem( new ItemDto( 0L, "Water", "H2O", 3.50 ) );
+        final ItemDto newItem2 = itemService.addItem( new ItemDto( 0L, "Coffee", "Brown liquid", 5.50 ) );
+        expectedInventory.getItemQuantities().put( newItem.getId(), 5 );
+        expectedInventory.getItemQuantities().put( newItem2.getId(), 3 );
+
+        inventoryRepository.deleteAll();
+
+        final InventoryDto updates = new InventoryDto();
+        updates.getItemQuantities().put( newItem2.getId(), 3 );
+        updates.getItemQuantities().put( newItem.getId(), 5 );
+        mvc.perform( put( "/api/inventory" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updates ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() )
+                .andExpect( content().string( TestUtils.asJsonString( expectedInventory ) ) );
+
+        updates.getItemQuantities().put( newItem2.getId() + 1L, 3 );
+        mvc.perform( put( "/api/inventory" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updates ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isBadRequest() );
+    }
+
+}
---
+++ wolf-cafe-backend/src/test/java/edu/ncsu/csc326/wolfcafe/controller/ItemControllerTest.java
@@ -0,0 +1,262 @@
+package edu.ncsu.csc326.wolfcafe.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+import edu.ncsu.csc326.wolfcafe.TestUtils;
+import edu.ncsu.csc326.wolfcafe.dto.ItemDto;
+import edu.ncsu.csc326.wolfcafe.repository.ItemRepository;
+
+/**
+ * Tests for ItemController, including creating and getting items
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ItemControllerTest {
+
+    /**
+     * Mock mvc for making test api calls
+     */
+    @Autowired
+    private MockMvc                   mvc;
+
+    /**
+     * Connection to the database to delete items
+     */
+    @Autowired
+    private ItemRepository            itemRepository;
+
+    /**
+     * Used to convert objects to Json
+     */
+    private static final ObjectMapper MAPPER           = new ObjectMapper();
+
+    /**
+     * Api endpoint under test
+     */
+    private static final String       API_PATH         = "/api/items";
+    /**
+     * Encoding for performing api calls
+     */
+    private static final String       ENCODING         = "utf-8";
+    /**
+     * Name of test item
+     */
+    private static final String       ITEM_NAME        = "Coffee";
+    /**
+     * Description of test item
+     */
+    private static final String       ITEM_DESCRIPTION = "Coffee is life";
+    /**
+     * Price of test item
+     */
+    private static final double       ITEM_PRICE       = 3.25;
+
+    /**
+     * Sets up testing environment by deleting all existing items to avoid
+     * duplicate names
+     */
+    @BeforeEach
+    public void setUp () {
+        // Delete all items in the current repository
+        itemRepository.deleteAll();
+    }
+
+    /**
+     * Test for creating a new item with a POST request
+     *
+     * @throws Exception
+     *             in case of an unexpected error
+     */
+    @Test
+    @WithMockUser ( username = "manager", roles = "MANAGER" )
+    public void testCreateItem () throws Exception {
+        // Create ItemDto with all contents but the id
+        final ItemDto itemDto = new ItemDto();
+        itemDto.setName( ITEM_NAME );
+        itemDto.setDescription( ITEM_DESCRIPTION );
+        itemDto.setPrice( ITEM_PRICE );
+
+        final String json = MAPPER.writeValueAsString( itemDto );
+
+        mvc.perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                .content( json ).accept( MediaType.APPLICATION_JSON ) ).andExpect( status().isCreated() )
+                .andExpect( jsonPath( "$.name", Matchers.equalTo( ITEM_NAME ) ) )
+                .andExpect( jsonPath( "$.description", Matchers.equalTo( ITEM_DESCRIPTION ) ) )
+                .andExpect( jsonPath( "$.price", Matchers.equalTo( ITEM_PRICE ) ) );
+
+        final ItemDto duplicateDto = new ItemDto();
+        duplicateDto.setName( ITEM_NAME );
+        duplicateDto.setDescription( ITEM_DESCRIPTION );
+        duplicateDto.setPrice( ITEM_PRICE );
+
+        final String json1 = MAPPER.writeValueAsString( duplicateDto );
+
+        mvc.perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                .content( json1 ).accept( MediaType.APPLICATION_JSON ) ).andExpect( status().isConflict() );
+
+    }
+
+    /**
+     * Testing that non-staff users cannot create items
+     *
+     * @throws Exception
+     *             in case of unexpected error
+     */
+    @Test
+    public void testCreateItemNotStaff () throws Exception {
+        // Create ItemDto with all contents but the id
+        final ItemDto itemDto = new ItemDto();
+        itemDto.setName( ITEM_NAME );
+        itemDto.setDescription( ITEM_DESCRIPTION );
+        itemDto.setPrice( ITEM_PRICE );
+
+        final String json = MAPPER.writeValueAsString( itemDto );
+
+        mvc.perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                .content( json ).accept( MediaType.APPLICATION_JSON ) ).andExpect( status().isUnauthorized() );
+    }
+
+    /**
+     * Testing retrieving an item with a GET api call
+     *
+     * @throws Exception
+     *             in case of unexpected error
+     */
+    @Test
+    @WithMockUser ( username = "manager", roles = "MANAGER" )
+    public void testGetItemById () throws Exception {
+        final ItemDto itemDto = new ItemDto();
+        itemDto.setName( ITEM_NAME );
+        itemDto.setDescription( ITEM_DESCRIPTION );
+        itemDto.setPrice( ITEM_PRICE );
+
+        final String json = "";
+
+        final Long itemId = JsonPath.parse( mvc
+                .perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                        .content( TestUtils.asJsonString( itemDto ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isCreated() ).andReturn().getResponse().getContentAsString() )
+                .read( "$.id", Long.class );
+
+        mvc.perform( get( API_PATH + "/" + itemId ).contentType( MediaType.APPLICATION_JSON )
+                .characterEncoding( ENCODING ).content( json ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() ).andExpect( jsonPath( "$.name", Matchers.equalTo( ITEM_NAME ) ) )
+                .andExpect( jsonPath( "$.description", Matchers.equalTo( ITEM_DESCRIPTION ) ) )
+                .andExpect( jsonPath( "$.price", Matchers.equalTo( ITEM_PRICE ) ) );
+
+        // Test getting an item that does not exist
+        mvc.perform( get( API_PATH + "/" + 123123L ).contentType( MediaType.APPLICATION_JSON )
+                .characterEncoding( ENCODING ).content( json ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isNotFound() );
+    }
+
+    /**
+     * Tests get on the items endpoint with no items
+     *
+     * @throws Exception
+     *             in case of unexpected error
+     */
+    @Test
+    @WithMockUser ( username = "customer", roles = "CUSTOMER" )
+    public void testGetItems () throws Exception {
+        final String items = mvc.perform( get( API_PATH ) ).andDo( print() ).andExpect( status().isOk() ).andReturn()
+                .getResponse().getContentAsString();
+        assertEquals( "[]", items );
+    }
+
+    /**
+     * Tests delete on the items endpoint, by creating an item and then calling
+     * the endpoint to delete the item
+     *
+     * @throws Exception
+     *             in case of unexpected error
+     */
+    @Test
+    @WithMockUser ( username = "manager", roles = "MANAGER" )
+    public void testDeleteItem () throws Exception {
+        final ItemDto itemDto = new ItemDto( 0L, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE );
+
+        final Long itemId = JsonPath.parse( mvc
+                .perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                        .content( TestUtils.asJsonString( itemDto ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isCreated() ).andReturn().getResponse().getContentAsString() )
+                .read( "$.id", Long.class );
+
+        mvc.perform( get( API_PATH + "/" + itemId ).contentType( MediaType.APPLICATION_JSON )
+                .characterEncoding( ENCODING ).content( "" ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() ).andExpect( jsonPath( "$.id" ).value( itemId ) )
+                .andExpect( jsonPath( "$.name", Matchers.equalTo( ITEM_NAME ) ) )
+                .andExpect( jsonPath( "$.description", Matchers.equalTo( ITEM_DESCRIPTION ) ) )
+                .andExpect( jsonPath( "$.price", Matchers.equalTo( ITEM_PRICE ) ) );
+
+        mvc.perform( delete( API_PATH + "/" + itemId ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() );
+
+        // Verify the old item id no longer exists
+        mvc.perform( get( API_PATH + "/" + itemId ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isNotFound() );
+
+        // Test deleting an item that does not exist
+        mvc.perform( delete( API_PATH + "/" + itemId ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isNotFound() );
+    }
+
+    /**
+     * Tests updating an item through a put call, by creating an item, updating
+     * it, and then calling the endpoint to get the updated item
+     *
+     * @throws Exception
+     *             in case of unexpected error
+     */
+    @Test
+    @WithMockUser ( username = "manager", roles = "MANAGER" )
+    public void testUpdateItem () throws Exception {
+        // Create initial item
+        final ItemDto itemDto = new ItemDto( 0L, ITEM_NAME, ITEM_DESCRIPTION, ITEM_PRICE );
+
+        final Long itemId = JsonPath.parse( mvc
+                .perform( post( API_PATH ).contentType( MediaType.APPLICATION_JSON ).characterEncoding( ENCODING )
+                        .content( TestUtils.asJsonString( itemDto ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isCreated() ).andReturn().getResponse().getContentAsString() )
+                .read( "$.id", Long.class );
+
+        // Update item
+        final ItemDto updatedItemDto = new ItemDto( itemId, "Mocha", "idk what a Mocha is I don't drink coffee",
+                19.99 );
+
+        mvc.perform( put( API_PATH + "/" + itemId ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updatedItemDto ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isOk() );
+
+        // Verify updated item
+        mvc.perform( get( API_PATH + "/" + itemId ).accept( MediaType.APPLICATION_JSON ) ).andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.id" ).value( itemId ) ).andExpect( jsonPath( "$.name" ).value( "Mocha" ) )
+                .andExpect( jsonPath( "$.description" ).value( "idk what a Mocha is I don't drink coffee" ) )
+                .andExpect( jsonPath( "$.price" ).value( 19.99 ) );
+
+        // Test updating an item that does not exist
+        mvc.perform( put( API_PATH + "/" + 1345663L ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( updatedItemDto ) ).accept( MediaType.APPLICATION_JSON ) )
+                .andExpect( status().isNotFound() );
+    }
+}
