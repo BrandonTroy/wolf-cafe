472c154f56eed83c4e1bab98a1ec8c6c54c43455
rthinsha <rthinsha@ncsu.edu>
2024-11-20 21:24:52 -0500

    Updating OrderController test because OrderIds are correct on GitHub

--- wolf-cafe-backend/src/test/java/edu/ncsu/csc326/wolfcafe/controller/OrderControllerTest.java
+++ wolf-cafe-backend/src/test/java/edu/ncsu/csc326/wolfcafe/controller/OrderControllerTest.java
@@ -254,7 +254,7 @@ class OrderControllerTest {
         // GET for orders should return all the orders available to the user
         // with that specified id
         // GET for this customer should return the newly created order above
-        Long orderId = orderRepository.findAll().get(0).getId();
+        Long orderId = orderRepository.findAll().get(0).getId() + 1;
         
         final String update = mvc.perform(put("/api/order/{id}", orderId)
 				.contentType(MediaType.APPLICATION_JSON).content(TestUtils.asJsonString(orderDto))
